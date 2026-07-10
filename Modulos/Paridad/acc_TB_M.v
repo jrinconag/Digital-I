2af8da40f3ba06a14940c3b43bfa88046c1a930b
jrinconag <jrinconag@unal.edu.co>
2026-07-10 15:19:23 -0500

    Add files via upload

---
+++ Modulos/Paridad/acc_TB_M.v
@@ -0,0 +1,51 @@
+`timescale 1ns / 1ps
+`define SIMULATION
+
+module acc_TB_M;
+
+   reg clk;
+   reg addi;
+   reg rst;
+   wire [5:0]i;
+
+   acc_M uut( .clk(clk) , .i(i) , .addi(addi) , .rst(rst));
+
+   parameter PERIOD          = 20;
+   parameter real DUTY_CYCLE = 0.5;
+   parameter OFFSET          = 0;
+
+   initial  begin  // Process for clk
+     #OFFSET;
+     forever
+       begin
+         clk = 1'b0;
+         #(PERIOD-(PERIOD*DUTY_CYCLE)) clk = 1'b1;
+         #(PERIOD*DUTY_CYCLE);
+       end
+   end
+
+   initial begin // Reset the system, Start the image capture process
+      rst = 0; addi = 0; 
+   end
+
+   reg [3:0] k;
+   initial begin // Reset the system, Start the image capture process
+        #20 rst = 1;
+        @ (posedge clk);
+        @ (negedge clk);
+        rst = 0;
+        @ (posedge clk);
+        @ (negedge clk);
+        addi = 1;
+       for(k=0; k<10; k=k+1) begin
+         @ (posedge clk);
+       end
+   end
+
+
+   initial begin: TEST_CASE
+     $dumpfile("acc_TB_M.vcd");
+     $dumpvars(-1, uut);
+     #(1000) $finish;
+   end
+endmodule
---
+++ Modulos/Paridad/shiftR_7_M.v
@@ -0,0 +1,18 @@
+module shiftR_7_M (clk , sh, rst, in_A, s_A);
+  input clk;
+  input sh;
+  input rst;
+  input [6:0] in_A;
+  output reg [6:0] s_A;
+
+always @(negedge clk)
+  if (rst)
+   s_A <= {in_A};
+  else
+     begin
+      if (sh)
+        s_A <= {1'b0, s_A[6:1]};
+      else
+        s_A <= s_A;
+     end
+endmodule
